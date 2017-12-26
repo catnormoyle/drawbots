620b6ee994f969e6ad85532c792063b090236add
Ray Walker <scottiew@gmail.com>
2017-12-26 13:32:10 -0600

    Add files via upload

--- serveRover5.py
+++ serveRover5.py
@@ -4,6 +4,8 @@ import sys
 import time
 import serial
 import RPi.GPIO as GPIO
+import picamera
+
 sys.path.append(r'/home/pi')
 
 app = Flask(__name__)
@@ -22,17 +24,38 @@ def hello():
       'time': timeString
       }
    return render_template('main.html', **templateData)
+   
+@app.route("/cam")
+def cam():
+   now = datetime.datetime.now()
+   timeString = now.strftime("%Y-%m-%d %H:%M")
+   templateData = {
+      'title' : 'Rover 5',
+      'time': timeString
+      }
+   return render_template('camtest.html', **templateData)
  
+@app.route('/test/')
+def vid():
+   with picamera.PiCamera() as camera:
+      stream = io.BytesIO()
+      for foo in camera.capture_continuous(stream, format='jpeg'):
+         stream.truncate()
+         stream.seek(0)
+         if process(stream):
+            break
   
 @app.route("/forward/")
 def forward():
-  print 'Calling forward!'
+  print 'Calling forward A!'
+  speed = request.args.get('speed', None)
+  print 'Calling forward!' + speed
   GPIO.setmode(GPIO.BCM)
   GPIO.setup(19, GPIO.OUT)
   GPIO.setup(20, GPIO.OUT)
   GPIO.output(19, GPIO.HIGH)
   GPIO.output(20, GPIO.HIGH)
-  time.sleep(float(sleep))
+  time.sleep(float(speed))
   GPIO.output(19, GPIO.LOW)
   GPIO.output(20, GPIO.LOW)
   return 'Click.'
@@ -41,12 +64,13 @@ def forward():
 @app.route("/backward/") 
 def backward(): 
   print 'Calling backward!' 
+  speed = request.args.get('speed', None)
   GPIO.setmode(GPIO.BCM)
   GPIO.setup(26, GPIO.OUT)
   GPIO.setup(21, GPIO.OUT)
   GPIO.output(26, GPIO.HIGH)
   GPIO.output(21, GPIO.HIGH)
-  time.sleep(float(sleep))
+  time.sleep(float(speed))
   GPIO.output(26, GPIO.LOW)
   GPIO.output(21, GPIO.LOW)
   return 'Click.'
@@ -55,12 +79,13 @@ def backward():
 @app.route("/right/")
 def right():
   print 'Calling right!'
+  speed = request.args.get('speed', None)  
   GPIO.setmode(GPIO.BCM)
   GPIO.setup(20, GPIO.OUT)
   GPIO.setup(26, GPIO.OUT)
   GPIO.output(20, GPIO.HIGH)
   GPIO.output(26, GPIO.HIGH)
-  time.sleep(float(sleep))
+  time.sleep(float(speed))
   GPIO.output(20, GPIO.LOW)
   GPIO.output(26, GPIO.LOW)
   return 'Click.'
@@ -68,12 +93,13 @@ def right():
 @app.route("/left/")
 def left():
   print 'Calling left!'
+  speed = request.args.get('speed', None)    
   GPIO.setmode(GPIO.BCM)
   GPIO.setup(19, GPIO.OUT)
   GPIO.setup(21, GPIO.OUT)
   GPIO.output(19, GPIO.HIGH)
   GPIO.output(21, GPIO.HIGH)
-  time.sleep(float(sleep))
+  time.sleep(float(speed))
   GPIO.output(19, GPIO.LOW)
   GPIO.output(21, GPIO.LOW)
   return 'Click.'
